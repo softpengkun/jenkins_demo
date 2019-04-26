10d6827dd67af0aaaf82929ca5cf420be64d4fde
softpengkun <244964268@qq.com>
2019-04-26 14:55:50 +0800

    项目初始化

---
+++ jenkins_demo/src/main/java/com/pk/jenkins_demo/TestController.java
@@ -0,0 +1,19 @@
+package com.pk.jenkins_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pengkun
+ * @date 2019/4/26 0026 14:37
+ * @description 测试jenkins
+ */
+@RestController
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String test(){
+        return "这是一个测试jenkins自动部署的测试demo";
+    }
+}
