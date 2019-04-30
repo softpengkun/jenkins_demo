b1d0238dacbbdfeb9b64802ab9e27db4da4033ac
softpengkun <244964268@qq.com>
2019-04-30 15:12:00 +0800

    888

--- jenkins_demo/src/main/java/com/pk/jenkins_demo/TestController.java
+++ jenkins_demo/src/main/java/com/pk/jenkins_demo/TestController.java
@@ -15,6 +15,6 @@ public class TestController {
     @GetMapping("/test")
     public String test(){
         System.out.println("合并");
-        return "这是一个测试jenkins自动部署的测试demo8888";
+        return "这是一个测试jenkins自动部署的测试demo999";
     }
 }
